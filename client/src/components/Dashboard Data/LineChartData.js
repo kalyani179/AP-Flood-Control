69343ab64714c6d8e29d77e35f6366941536dc62
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-09-14 10:36:36 +0530

    modified data

--- client/src/components/Dashboard Data/LineChartData.js
+++ client/src/components/Dashboard Data/LineChartData.js
@@ -69,4 +69,109 @@ export const LineChartData = {
             }
         ],
     },
+    'Ward 3': {
+        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
+        datasets: [
+            {
+                label: "Garbage Pileup",
+                data: [18, 10, 3],  // Different data for Ward 2
+                fill: false,
+                borderColor: "rgba(255, 159, 64, 1)",
+            },
+            {
+                label: "Submerged Vehicle",
+                data: [4, 2, 1],
+                fill: false,
+                borderColor: "rgba(54, 162, 235, 1)",
+            },
+            {
+                label: "Submerged Building",
+                data: [6, 3, 0],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            },
+            {
+                label: "Mosquito Hotspot",
+                data: [10, 7, 1],
+                fill: false,
+                borderColor: "rgba(255, 99, 132, 1)",
+            },
+            {
+                label: "Silt Area",
+                data: [12, 9, 5],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            }
+        ],
+    },
+    'Ward 4': {
+        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
+        datasets: [
+            {
+                label: "Garbage Pileup",
+                data: [18, 10, 3],  // Different data for Ward 2
+                fill: false,
+                borderColor: "rgba(255, 159, 64, 1)",
+            },
+            {
+                label: "Submerged Vehicle",
+                data: [4, 2, 1],
+                fill: false,
+                borderColor: "rgba(54, 162, 235, 1)",
+            },
+            {
+                label: "Submerged Building",
+                data: [6, 3, 0],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            },
+            {
+                label: "Mosquito Hotspot",
+                data: [10, 7, 1],
+                fill: false,
+                borderColor: "rgba(255, 99, 132, 1)",
+            },
+            {
+                label: "Silt Area",
+                data: [12, 9, 5],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            }
+        ],
+    },
+    'Ward 5': {
+        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
+        datasets: [
+            {
+                label: "Garbage Pileup",
+                data: [18, 10, 3],  // Different data for Ward 2
+                fill: false,
+                borderColor: "rgba(255, 159, 64, 1)",
+            },
+            {
+                label: "Submerged Vehicle",
+                data: [4, 2, 1],
+                fill: false,
+                borderColor: "rgba(54, 162, 235, 1)",
+            },
+            {
+                label: "Submerged Building",
+                data: [6, 3, 0],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            },
+            {
+                label: "Mosquito Hotspot",
+                data: [10, 7, 1],
+                fill: false,
+                borderColor: "rgba(255, 99, 132, 1)",
+            },
+            {
+                label: "Silt Area",
+                data: [12, 9, 5],
+                fill: false,
+                borderColor: "rgba(75, 192, 192, 1)",
+            }
+        ],
+    }
 };

e586d43d81ab8cf685a2970895960fa60c277686
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-09-14 10:38:32 +0530

    modified chart data

--- client/src/components/Dashboard Data/LineChartData.js
+++ client/src/components/Dashboard Data/LineChartData.js
@@ -70,17 +70,17 @@ export const LineChartData = {
         ],
     },
     'Ward 3': {
-        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
+        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis
         datasets: [
             {
                 label: "Garbage Pileup",
-                data: [18, 10, 3],  // Different data for Ward 2
+                data: [26, 12, 4],  // Y-axis data for Garbage Pileup
                 fill: false,
                 borderColor: "rgba(255, 159, 64, 1)",
             },
             {
                 label: "Submerged Vehicle",
-                data: [4, 2, 1],
+                data: [6, 3, 0],
                 fill: false,
                 borderColor: "rgba(54, 162, 235, 1)",
             },
@@ -92,54 +92,19 @@ export const LineChartData = {
             },
             {
                 label: "Mosquito Hotspot",
-                data: [10, 7, 1],
+                data: [15, 5, 2],
                 fill: false,
                 borderColor: "rgba(255, 99, 132, 1)",
             },
             {
                 label: "Silt Area",
-                data: [12, 9, 5],
+                data: [14, 12, 6],
                 fill: false,
                 borderColor: "rgba(75, 192, 192, 1)",
             }
         ],
     },
     'Ward 4': {
-        labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
-        datasets: [
-            {
-                label: "Garbage Pileup",
-                data: [18, 10, 3],  // Different data for Ward 2
-                fill: false,
-                borderColor: "rgba(255, 159, 64, 1)",
-            },
-            {
-                label: "Submerged Vehicle",
-                data: [4, 2, 1],
-                fill: false,
-                borderColor: "rgba(54, 162, 235, 1)",
-            },
-            {
-                label: "Submerged Building",
-                data: [6, 3, 0],
-                fill: false,
-                borderColor: "rgba(75, 192, 192, 1)",
-            },
-            {
-                label: "Mosquito Hotspot",
-                data: [10, 7, 1],
-                fill: false,
-                borderColor: "rgba(255, 99, 132, 1)",
-            },
-            {
-                label: "Silt Area",
-                data: [12, 9, 5],
-                fill: false,
-                borderColor: "rgba(75, 192, 192, 1)",
-            }
-        ],
-    },
-    'Ward 5': {
         labels: ['2024-09-08', '2024-09-09', '2024-09-10'],  // Date values for X-axis (can be the same or different)
         datasets: [
             {

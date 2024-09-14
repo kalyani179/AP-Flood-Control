950926124ab0eaa900a62e72a1cf568c28c0aed6
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-09-14 16:01:54 +0530

    dashboard changes

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -50,7 +50,7 @@ const Dashboard = () => {
     }, [selectedWard, selectedDate]);
 
     useEffect(() => {
-        fetch('https://ap-flood-control.onrender.com')
+        fetch('https://ap-flood-control.onrender.com/data')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
